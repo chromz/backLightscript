96e6fbf4d7db703a15e479a927f7e6321635e8a2
Rodrigo Custodio <jorocuva@gmail.com>
2018-02-04 13:20:53 -0600

    a

--- soundScript.sh
+++ soundScript.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-sink="$(pacmd info | grep "sink:.*$sinkName" | awk '{print $2}')"
+sink="$(pacmd list-sinks|awk '/\* index:/{ print $3 }')"
 
 
 if [[ $1 == -u ]]
 then
-	pactl set-sink-mute 0 false ; pactl set-sink-volume 0 +5%
+	pactl set-sink-mute $sink false ; pactl set-sink-volume $sink +5%
 else
 	if [[ $1 == -d ]]
 	then
-		pactl set-sink-mute 0 false ; pactl set-sink-volume 0 -5%
+		pactl set-sink-mute $sink false ; pactl set-sink-volume $sink -5%
 	else
 		if [[ $1 == -m ]]; then
-			pactl set-sink-mute 0 toggle
+			pactl set-sink-mute $sink toggle
 		fi
 	fi
 fi
