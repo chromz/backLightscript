2581fa3015b09e54ee4a89cf3817cbe3c98fc990
Rodrigo Custodio <jorocuva@gmail.com>
2018-02-16 17:48:55 -0600

    added 100% limit

--- soundScript.sh
+++ soundScript.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 sink="$(pacmd list-sinks|awk '/\* index:/{ print $3 }')"
-
+current="$(pactl list sinks | perl -000ne 'if(/#1/){/Volume:.*?(\d+)%/; (print $1)}')"
 
 if [[ $1 == -u ]]
 then
-	pactl set-sink-mute $sink false ; pactl set-sink-volume $sink +5%
+  if [[ $((current + 5 )) -gt 100 ]]
+  then
+    pactl set-sink-mute $sink false ; pactl set-sink-volume $sink 100%
+  else
+    pactl set-sink-mute $sink false ; pactl set-sink-volume $sink +5%
+  fi
 else
 	if [[ $1 == -d ]]
 	then
-		pactl set-sink-mute $sink false ; pactl set-sink-volume $sink -5%
+		if [[ $((current + 5 )) -lt 0 ]]
+    then
+      pactl set-sink-mute $sink false ; pactl set-sink-volume $sink 0%
+    else
+      pactl set-sink-mute $sink false ; pactl set-sink-volume $sink -5%
+    fi
 	else
 		if [[ $1 == -m ]]; then
 			pactl set-sink-mute $sink toggle
